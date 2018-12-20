a93d35388b3380027f9dbabcb2359cfbe838d64b
prathamesh <prathamesh341@gmail.com>
2018-12-21 02:25:21 +0530

    initial Commit 2

--- venv/app/app.py
+++ venv/app/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4e496f51234e81ea776d73a11a43ecc8 '
 
 posts =[
     {
@@ -27,6 +30,16 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', posts =posts, title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ =="__main__":
     app.run(debug=True)
